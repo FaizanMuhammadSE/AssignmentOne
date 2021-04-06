7842184e7692ec42ad76bd356906b3641e75c2c5
FaizanMuhammadSE <bsef18m023@pucit.edu.pk>
2021-04-06 09:53:41 +0500

    Images added

--- AssignmentOne/app/src/main/java/com/example/assignmentone/MainActivity.java
+++ AssignmentOne/app/src/main/java/com/example/assignmentone/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.assignmentone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -25,5 +28,16 @@ public class MainActivity extends AppCompatActivity {
         arrayAdapter=new ArrayAdapter<String>(this, android.R.layout.simple_expandable_list_item_1,alphabets);
         screenView=findViewById(R.id.myView);
         screenView.setAdapter(arrayAdapter);
+
+
+        screenView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent=new Intent(MainActivity.this,ImageShow.class);
+                intent.putExtra("ClickedAlphabet",alphabets.get(position));
+                startActivity(intent);
+            }
+        });
+
     }
 }
